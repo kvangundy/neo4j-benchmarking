1cadb7cc6b0b04e25974c34ba15b983b6f803cea
Kevin Van Gundy <kvangundy@users.noreply.github.com>
2016-09-01 12:56:36 -0700

    Create run-billion-node-test.sh

---
+++ social-graph-bacon/billion-node/run-billion-node-test.sh
@@ -0,0 +1,4 @@
+# modify path to your neo4j install as well as where you've stored the scripts
+# copy this into your terminal
+
+Desktop/neo4j-enterprise-3.0.3/bin/neo4j-shell -file Desktop/benchmark-blogpost/billion-node-test.cql >Desktop/billion-node-results.txt
