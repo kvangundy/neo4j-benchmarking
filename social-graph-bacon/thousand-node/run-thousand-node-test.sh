5b040fd349a623f5b4e33573eef4479f035ca782
kvangundy <kevinvangundy@The-Librarian.local>
2016-09-01 16:48:13 -0700

    do shit

--- social-graph-bacon/thousand-node/run-thousand-node-test.sh
+++ social-graph-bacon/thousand-node/run-thousand-node-test.sh
@@ -2,7 +2,7 @@
 # copy this into your terminal
 Desktop/neo4j-enterprise-3.0.3/bin/neo4j start;
 sleep 15s;
-Desktop/neo4j-enterprise-3.0.3/bin/neo4j-shell -file Desktop/benchmark-blogpost/thousand-node-test.cql >Desktop/thousand-node-results.txt;
+Desktop/neo4j-enterprise-3.0.3/bin/neo4j-shell -file Documents/neo4j-benchmarking/social-graph-bacon/thousand-node/ten-thousand-node-test.cql >Desktop/thousand-node-results.txt;
 Desktop/neo4j-enterprise-3.0.3/bin/neo4j stop;
 #empties the billion node graph when done
 rm -iR Desktop/neo4j-enterprise-3.0.3/data/databases/graph.db;
