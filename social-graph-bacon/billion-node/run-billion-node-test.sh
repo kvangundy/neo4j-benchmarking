b93247abf42e342e6191f73b286ff89d8a16d2e9
Kevin Van Gundy <kvangundy@users.noreply.github.com>
2016-09-01 13:03:09 -0700

    Update run-billion-node-test.sh

--- social-graph-bacon/billion-node/run-billion-node-test.sh
+++ social-graph-bacon/billion-node/run-billion-node-test.sh
@@ -2,3 +2,7 @@
 # copy this into your terminal
 
 Desktop/neo4j-enterprise-3.0.3/bin/neo4j-shell -file Desktop/benchmark-blogpost/billion-node-test.cql >Desktop/billion-node-results.txt
+
+#empties the billion node graph when done
+
+rm -iR Desktop/neo4j-enterprise-3.0.3/data/databases/graph.db
