31b75fd8f34226f269ab441343b507ec5288c489
Kevin Van Gundy <kvangundy@users.noreply.github.com>
2016-09-01 13:32:20 -0700

    Update run-thousand-node-test.sh

--- social-graph-bacon/thousand-node/run-thousand-node-test.sh
+++ social-graph-bacon/thousand-node/run-thousand-node-test.sh
@@ -1,4 +1,8 @@
 # modify path to your neo4j install as well as where you've stored the scripts
 # copy this into your terminal
-
-Desktop/neo4j-enterprise-3.0.3/bin/neo4j-shell -file Desktop/benchmark-blogpost/thousand-node-test.cql >Desktop/thousand-node-results.txt
+Desktop/neo4j-enterprise-3.0.3/bin/neo4j start;
+sleep 15s;
+Desktop/neo4j-enterprise-3.0.3/bin/neo4j-shell -file Desktop/benchmark-blogpost/thousand-node-test.cql >Desktop/thousand-node-results.txt;
+Desktop/neo4j-enterprise-3.0.3/bin/neo4j stop;
+#empties the billion node graph when done
+rm -iR Desktop/neo4j-enterprise-3.0.3/data/databases/graph.db;
